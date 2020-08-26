41e1f150ec0235dc883500b911a612827003616d
藤原立暉 <r9.20important@icloud.com>
2020-08-27 07:41:15 +0900

    master

---
+++ blog_list.php
@@ -0,0 +1,115 @@
+
+
+<?php get_header();?>
+
+		<?php get_template_part('content','menu');?>
+
+		<div id="main">
+
+			<!-- blog_list -->
+			<section id="blog_list" class="site-width">
+				<h1 class="title">BLOG</h1>
+				<div id="content" class="article">
+					<article class="article-item">
+						<h2 class="article-title"><a href="blog.html">サンプル記事１サンプル記事１</a></h2>
+						<h3 style="font-size:80%;">crazy-wp　2014年11月10日　カテゴリー： ブログ</h3>
+						<img src="img/photo2.jpeg" class="article-img">
+						<p class="article-body">
+							サンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメント
+							サンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメント
+						</p>
+					</article>
+					<article class="article-item">
+						<h2 class="article-title"><a href="blog.html">サンプル記事２サンプル記事２</a></h2>
+						<h3 style="font-size:80%;">crazy-wp　2014年11月10日　カテゴリー： ブログ</h3>
+						<img src="img/photo4.jpeg" class="article-img">
+						<p class="article-body">
+							サンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメント
+							サンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメント
+						</p>
+					</article>
+					<article class="article-item">
+						<h2 class="article-title"><a href="blog.html">サンプル記事３サンプル記事３</a></h2>
+						<h3 style="font-size:80%;">crazy-wp　2014年11月10日　カテゴリー： ブログ</h3>
+						<img src="img/photo5.jpeg" class="article-img">
+						<p class="article-body">
+							サンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメント
+							サンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメントサンプルコメント
+						</p>
+					</article>
+					<div class="pagenation">
+						<ul>
+							<li class="active">1</li>
+							<li><a href="">2</a></li>
+							<li class="next"><a href="">NEXT</a></li>
+						</ul>
+					</div>
+				</div>
+				<!-- <div id="sidebar" class="sidebar-widget">
+					<div class="sidebar-item">
+						<h2>新着記事</h2>
+						<ul>
+							<li>
+								<a href="">Hello world!</a>
+							</li>
+							<li>
+								<a href="">Hello world!</a>
+							</li>
+							<li>
+								<a href="">ブログサンプルタイトル</a>
+							</li>
+							<li>
+								<a href="">サンプルブログ投稿</a>
+							</li>
+							<li>
+								<a href="">Hello world!</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sidebar-item">
+						<h2>カテゴリー</h2>
+						<ul>
+							<li>
+								<a href="">ブログ(10)</a>
+							</li>
+							<li>
+								<a href="">未分類(2)</a>
+							</li>
+							<li>
+								<a href="">ニュース(27)</a>
+							</li>
+							<li>
+								<a href="">イベント(8)</a>
+							</li>
+							<li>
+								<a href="">その他(3)</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sidebar-item">
+						<h2>アーカイブ</h2>
+						<ul>
+							<li>
+								<a href="">2015.12.01</a>
+							</li>
+							<li>
+								<a href="">2015.11.28</a>
+							</li>
+							<li>
+								<a href="">2015.11.22</a>
+							</li>
+							<li>
+								<a href="">2015.11.18</a>
+							</li>
+							<li>
+								<a href="">2015.10.09</a>
+							</li>
+						</ul>
+					</div>
+				</div> -->
+                <?php get_sidebar();?>
+			</section>
+
+
+		</div>
+		<?php get_footer();?>
---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+$custom_header_defaults = array(
+    'default-image' => get_bloginfo('template_url').'/images/headers/logo.png',
+    'header-text' => false,
+);
+add_theme_support('custom-header',$custom_header_defaults);
+
+// カスタムメニュー使用
+register_nav_menu('mainmenu','メインメニュー');
+?>
