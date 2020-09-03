4c6177bdead064aa4e207d2e5cc2531d38f0766e
藤原立暉 <r9.20important@icloud.com>
2020-09-04 06:58:39 +0900

    no message

--- blog_list.php
+++ blog_list.php
@@ -1,5 +1,3 @@
-
-
 <?php get_header();?>
 
 		<?php get_template_part('content','menu');?>
@@ -44,69 +42,21 @@
 							<li class="next"><a href="">NEXT</a></li>
 						</ul>
 					</div>
+
+					<?php if (have_posts()) : ?>
+                <?php while (have_posts()) : the_post();?>
+                <article class="article-item">
+						<h2 class="article-title"><a href=""><?php the_permalink();?><?php the_title();?></a></h2>
+						<h3 style="font-size:80%;"><?php the_author_nickname();?><?php the_time("Y年m月j日");?><?php single_cat_title('カテゴリー:');?></h3>
+						<img src="img/photo2.jpeg" class="article-img">
+						<p class="article-body">
+                            <?php the_content();?>
+						</p>
+					</article>
+                <?php endwhile; ?>
+				<?php if (function_exists("pagination")) pagination($addition_loop->max_num_pages); ?>
 				</div>
-				<!-- <div id="sidebar" class="sidebar-widget">
-					<div class="sidebar-item">
-						<h2>新着記事</h2>
-						<ul>
-							<li>
-								<a href="">Hello world!</a>
-							</li>
-							<li>
-								<a href="">Hello world!</a>
-							</li>
-							<li>
-								<a href="">ブログサンプルタイトル</a>
-							</li>
-							<li>
-								<a href="">サンプルブログ投稿</a>
-							</li>
-							<li>
-								<a href="">Hello world!</a>
-							</li>
-						</ul>
-					</div>
-					<div class="sidebar-item">
-						<h2>カテゴリー</h2>
-						<ul>
-							<li>
-								<a href="">ブログ(10)</a>
-							</li>
-							<li>
-								<a href="">未分類(2)</a>
-							</li>
-							<li>
-								<a href="">ニュース(27)</a>
-							</li>
-							<li>
-								<a href="">イベント(8)</a>
-							</li>
-							<li>
-								<a href="">その他(3)</a>
-							</li>
-						</ul>
-					</div>
-					<div class="sidebar-item">
-						<h2>アーカイブ</h2>
-						<ul>
-							<li>
-								<a href="">2015.12.01</a>
-							</li>
-							<li>
-								<a href="">2015.11.28</a>
-							</li>
-							<li>
-								<a href="">2015.11.22</a>
-							</li>
-							<li>
-								<a href="">2015.11.18</a>
-							</li>
-							<li>
-								<a href="">2015.10.09</a>
-							</li>
-						</ul>
-					</div>
-				</div> -->
+				
                 <?php get_sidebar();?>
 			</section>
 
---
+++ category.php
@@ -0,0 +1,22 @@
+
+<?php get_header(); ?>
+
+<?php get_template_part('content','menu'); ?>
+
+		<div id="main">
+		<section id="blog_list" class="site-width">
+				<h1 class="title">BLOG</h1>
+				<div id="content" class="article">
+					
+					<?php get_template_part('loop'); ?>
+					<?php if (function_exists("pagination")) pagination($additional_loop->max_num_pages); ?>
+				</div>
+				
+			<!-- blog_list -->
+			<?php get_sidebar();?>
+					
+			</section>
+
+
+		</div>
+		<?php get_footer(); ?>
